7f4e3e44031df5d98ecac38052a85893f4737ced
ByLuis360 <usr2477@salesianos-lacuesta.net>
2025-03-23 18:23:31 +0000

    feat: SQL insert for table Team

---
+++ backend/src/main/resources/sql/data.sql
@@ -0,0 +1,142 @@
+-- competitions --
+
+INSERT INTO competitions (competitionName) VALUES 
+('La Liga'), 
+('Premier League'), 
+('Serie A'), 
+('Bundesliga'), 
+('Ligue 1'), 
+('Champions League');
+
+-- equipos --
+INSERT INTO teams (country, teamName, sport) VALUES 
+
+-- La Liga --
+
+('España', 'Barcelona', 'Fútbol'),
+('España', 'Atlético de Madrid', 'Fútbol'),
+('España', 'Real Madrid', 'Fútbol'),
+('España', 'Girona', 'Fútbol'),
+('España', 'Athletic', 'Fútbol'),
+('España', 'Real Sociedad', 'Fútbol'),
+('España', 'Betis', 'Fútbol'),
+('España', 'Valencia C.F.', 'Fútbol'),
+('España', 'Villarreal', 'Fútbol'),
+('España', 'Sevilla', 'Fútbol'),
+('España', 'Getafe', 'Fútbol'),
+('España', 'Rayo Vallecano', 'Fútbol'),
+('España', 'Osasuna', 'Fútbol'),
+('España', 'U.D. Las Palmas', 'Fútbol'),
+('España', 'Alavés', 'Fútbol'),
+('España', 'R.C.D. Mallorca', 'Fútbol'),
+('España', 'Celta de Vigo', 'Fútbol'),
+('España', 'Valladolid', 'Fútbol'),
+('España', 'Leganés', 'Fútbol'),
+('España', 'RCD Espanyol', 'Fútbol'),
+
+-- Premier League --
+
+('England', 'Arsenal', 'Football'),
+('England', 'Aston Villa', 'Football'),
+('England', 'Bournemouth', 'Football'),
+('England', 'Brentford', 'Football'),
+('England', 'Brighton', 'Football'),
+('England', 'Chelsea', 'Football'),
+('England', 'Crystal Palace', 'Football'),
+('England', 'Everton', 'Football'),
+('England', 'Fulham', 'Football'),
+('England', 'Ipswich', 'Football'),
+('England', 'Leicester City', 'Football'),
+('England', 'Liverpool', 'Football'),
+('England', 'Manchester City', 'Football'),
+('England', 'Manchester United', 'Football'),
+('England', 'Newcastle', 'Football'),
+('England', 'Nottingham Forest', 'Football'),
+('England', 'Southampton', 'Football'),
+('England', 'Tottenham', 'Football'),
+('England', 'West Ham', 'Football'),
+('England', 'Wolves', 'Football'),
+
+-- Serie A --
+
+('Italia', 'Atalanta', 'Fútbol'),
+('Italia', 'Bolonia', 'Fútbol'),
+('Italia', 'Cagliari', 'Fútbol'),
+('Italia', 'Como', 'Fútbol'),
+('Italia', 'Empoli', 'Fútbol'),
+('Italia', 'Fiorentina', 'Fútbol'),
+('Italia', 'Genoa', 'Fútbol'),
+('Italia', 'Hellas Verona', 'Fútbol'),
+('Italia', 'Inter', 'Fútbol'),
+('Italia', 'Juventus', 'Fútbol'),
+('Italia', 'Lazio', 'Fútbol'),
+('Italia', 'Lecce', 'Fútbol'),
+('Italia', 'Milan', 'Fútbol'),
+('Italia', 'Monza', 'Fútbol'),
+('Italia', 'Napoli', 'Fútbol'),
+('Italia', 'Parma', 'Fútbol'),
+('Italia', 'Roma', 'Fútbol'),
+('Italia', 'Torino', 'Fútbol'),
+('Italia', 'Udinese', 'Fútbol'),
+('Italia', 'Venezia', 'Fútbol'),
+
+-- BundesLiga --
+
+('Alemania', 'Augsburgo', 'Fútbol'),
+('Alemania', 'Leverkusen', 'Fútbol'),
+('Alemania', 'Bayern de Múnich', 'Fútbol'),
+('Alemania', 'Bochum', 'Fútbol'),
+('Alemania', 'B. Dortmund', 'Fútbol'),
+('Alemania', 'B. Mönchengladbach', 'Fútbol'),
+('Alemania', 'Frankfurt', 'Fútbol'),
+('Alemania', 'Friburgo', 'Fútbol'),
+('Alemania', 'Heidenheim', 'Fútbol'),
+('Alemania', 'Hoffenheim', 'Fútbol'),
+('Alemania', 'KSV Holstein', 'Fútbol'),
+('Alemania', 'Mainz 05', 'Fútbol'),
+('Alemania', 'RB Leipzig', 'Fútbol'),
+('Alemania', 'St. Pauli', 'Fútbol'),
+('Alemania', 'Stuttgart', 'Fútbol'),
+('Alemania', 'FC Unión Berlín', 'Fútbol'),
+('Alemania', 'Werder Bremen', 'Fútbol'),
+('Alemania', 'Wolfsburgo', 'Fútbol'),
+
+-- Ligue One --
+
+('Francia', 'Angers', 'Fútbol'),
+('Francia', 'Auxerre', 'Fútbol'),
+('Francia', 'Le Havre', 'Fútbol'),
+('Francia', 'Lens', 'Fútbol'),
+('Francia', 'LOSC', 'Fútbol'),
+('Francia', 'Mónaco', 'Fútbol'),
+('Francia', 'Montpellier', 'Fútbol'),
+('Francia', 'Nantes', 'Fútbol'),
+('Francia', 'Niza', 'Fútbol'),
+('Francia', 'Lyon', 'Fútbol'),
+('Francia', 'Marsella', 'Fútbol'),
+('Francia', 'PSG', 'Fútbol'),
+('Francia', 'Racing de Estrasburgo', 'Fútbol'),
+('Francia', 'Rennes', 'Fútbol'),
+('Francia', 'Saint-Étienne', 'Fútbol'),
+('Francia', 'Stade Brestois', 'Fútbol'),
+('Francia', 'Reims', 'Fútbol'),
+('Francia', 'Toulouse', 'Fútbol');
+
+-- another teams --
+
+('Portugal', 'Sporting Lisboa', 'Fútbol'),
+('Portugal', 'Benfica', 'Fútbol'),
+('Países Bajos', 'PSV', 'Fútbol'),
+('Países Bajos', 'Feyenoord', 'Fútbol'),
+('Austria', 'Sturm', 'Fútbol'),
+('Austria', 'RB Salzburg', 'Fútbol'),
+('Bélgica', 'Club Brujas', 'Fútbol'),
+('Escocia', 'Celtic F.C.', 'Fútbol'),
+('Ucrania', 'Shakhtar', 'Fútbol'),
+('República Checa', 'Sparta Praha', 'Fútbol'),
+('Suiza', 'Young Boys', 'Fútbol'),
+('Croacia', 'Dinamo Zagreb', 'Fútbol'),
+('Serbia', 'Estrella Roja', 'Fútbol'),
+('Eslovaquia', 'Slovan Bratislava', 'Fútbol');
+
+
