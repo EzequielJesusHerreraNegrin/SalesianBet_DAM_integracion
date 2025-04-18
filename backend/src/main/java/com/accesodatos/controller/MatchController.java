fda60258e5acbe727e56f954f27e6bbd8e2f6c78
ByLuis360 <usr2477@salesianos-lacuesta.net>
2025-04-18 22:43:57 +0100

    feat: creation of new endpoint for match

--- backend/src/main/java/com/accesodatos/controller/MatchController.java
+++ backend/src/main/java/com/accesodatos/controller/MatchController.java
@@ -8,6 +8,7 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,6 +27,7 @@ import com.accesodatos.service.MatchServiceImpl;
 
 @RestController
 @RequestMapping("/api/v1")
+@CrossOrigin(origins = "http://localhost:5173")
 public class MatchController {
 
 	private static final String MATCH_RESOURCE = "/matches";
